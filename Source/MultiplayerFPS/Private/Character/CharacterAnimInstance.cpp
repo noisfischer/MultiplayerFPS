5cb745213bb2d7368fccda9d0185ccc4dd1398a2
noisfischer <noisfischer@gmail.com>
2024-04-02 13:05:57 -0400

    AnimInstance Setup
    derived Animation Blueprint Created

---
+++ Source/MultiplayerFPS/Private/Character/CharacterAnimInstance.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/CharacterAnimInstance.h"
+#include "Character/FPSCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	PlayerRef = Cast<AFPSCharacter>(TryGetPawnOwner());
+}
+
+void UCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	// in case BeginPlay PlayerRef cast is unsuccessful
+	if(PlayerRef == nullptr)
+	{
+		PlayerRef = Cast<AFPSCharacter>(TryGetPawnOwner());
+	}
+	if(PlayerRef == nullptr)
+	{
+		return;
+	}
+
+	// Set character speed
+	FVector Velocity = PlayerRef->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	bIsInAir = PlayerRef->GetCharacterMovement()->IsFalling();
+	bIsAccelerating = PlayerRef->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0 ? true : false;
+}
---
+++ Source/MultiplayerFPS/Public/Character/CharacterAnimInstance.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CharacterAnimInstance.generated.h"
+
+UCLASS()
+class MULTIPLAYERFPS_API UCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	// Like BeginPlay()
+	virtual void NativeInitializeAnimation() override;
+	// Like Tick()
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	class AFPSCharacter* PlayerRef;
+	
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+};
