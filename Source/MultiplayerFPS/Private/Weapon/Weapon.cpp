fa278ec58ed44d383ecdf12d3330d60938262d9f
noisfischer <noisfischer@gmail.com>
2024-04-04 11:53:37 -0400

    Weapon Base Class
    Created base weapon class with skeletal mesh, sphere component, and EWeaponState. The sphere component will be used to detect whether a player pawn has overlapped. Its collision settings will be handled (replicated) by the server. EWeaponState will describe whether the weapon has been untouched (initial state), equipped, or dropped.

--- Source/MultiplayerFPS/Private/Weapon/Weapon.cpp
+++ Source/MultiplayerFPS/Private/Weapon/Weapon.cpp
@@ -2,23 +2,40 @@
 
 
 #include "Weapon/Weapon.h"
+#include "Components/SphereComponent.h"
 
-// Sets default values
 AWeapon::AWeapon()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
 
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	WeaponMesh->SetupAttachment(RootComponent);
+	SetRootComponent(WeaponMesh);
+	
+	WeaponMesh->SetCollisionResponseToAllChannels(ECR_Block);
+	WeaponMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore); // no collision with pawns
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision); // start with no collision
+
+	
+	AreaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreaSphere"));
+	AreaSphere->SetupAttachment(RootComponent);
+	
+	AreaSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
-// Called when the game starts or when spawned
 void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if(HasAuthority())
+	{
+		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		AreaSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	}
 }
 
-// Called every frame
 void AWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
--- Source/MultiplayerFPS/Public/Weapon/Weapon.h
+++ Source/MultiplayerFPS/Public/Weapon/Weapon.h
@@ -6,21 +6,38 @@
 #include "GameFramework/Actor.h"
 #include "Weapon.generated.h"
 
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial UMETA(DisplayName = "Initial State"),
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+	EWS_MAX UMETA(DisplayName = "DefaultMax")
+};
+
 UCLASS()
 class MULTIPLAYERFPS_API AWeapon : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AWeapon();
+	virtual void Tick(float DeltaTime) override;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	USkeletalMeshComponent* WeaponMesh;
+
+	// Used to detect overlap with characters - WILL BE HANDLED ON SERVER ONLY
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	class USphereComponent* AreaSphere;
+
+	UPROPERTY(VisibleAnywhere)
+	EWeaponState WeaponState;
+	
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
