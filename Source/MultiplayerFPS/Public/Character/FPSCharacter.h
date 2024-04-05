a20b3e471c70b4e1510228403d56e8ea11601f97
noisfischer <noisfischer@gmail.com>
2024-04-05 16:57:08 -0400

    Created getter for TurningInPlace state

--- Source/MultiplayerFPS/Public/Character/FPSCharacter.h
+++ Source/MultiplayerFPS/Public/Character/FPSCharacter.h
@@ -91,4 +91,5 @@ public:
 	FORCEINLINE float GetAO_Yaw() const { return AO_Yaw; }
 	FORCEINLINE float GetAO_Pitch() const { return AO_Pitch; }
 	AWeapon* GetEquippedWeapon();
+	FORCEINLINE ETurningInPlace GetTurningInPlace() const { return TurningInPlace; }
 };
