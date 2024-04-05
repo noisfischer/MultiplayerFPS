20e10238959d7c90221998c25e15644f973fe798
noisfischer <noisfischer@gmail.com>
2024-04-05 14:24:09 -0400

    FTransform Variable Init
    Will be used for Fabrik IK to ensure character's left hand will always be attached to a weapon socket

--- Source/MultiplayerFPS/Public/Character/CharacterAnimInstance.h
+++ Source/MultiplayerFPS/Public/Character/CharacterAnimInstance.h
@@ -54,4 +54,7 @@ private:
 
 	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	float AO_Pitch;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	FTransform LeftHandTransform;
 };
