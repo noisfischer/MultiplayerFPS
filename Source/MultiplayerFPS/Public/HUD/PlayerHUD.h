7d4c4a5d37e797a3495e7aaec75ce9ca192cde8d
noisfischer <noisfischer@gmail.com>
2024-04-07 17:52:56 -0400

    DrawHUD
    DrawCrosshairs does the heavy lifting by actually drawing the texture on the viewport. DrawHUD simply checks if each texture is valid then performs DrawCrosshairs on that specific texture

--- Source/MultiplayerFPS/Public/HUD/PlayerHUD.h
+++ Source/MultiplayerFPS/Public/HUD/PlayerHUD.h
@@ -28,6 +28,8 @@ public:
 private:
 	FHUDPackage HUDPackage;
 	
+	void DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter);
+	
 public:
 	FORCEINLINE void SetHUDPackage(const FHUDPackage& Package) { HUDPackage = Package; }
 };
