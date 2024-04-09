031aa4dacc87abc96cd697ab73db6bc9256d8347
noisfischer <noisfischer@gmail.com>
2024-04-09 10:55:53 -0400

    ProjectileBullet init

---
+++ Source/MultiplayerFPS/Public/Weapon/ProjectileBullet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/Projectile.h"
+#include "ProjectileBullet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERFPS_API AProjectileBullet : public AProjectile
+{
+	GENERATED_BODY()
+	
+};
