29f2078d8da0e4268249acecdc9bde5e595d18d4
noisfischer <noisfischer@gmail.com>
2024-04-09 16:43:35 -0400

    Respawn request in game mode

--- Source/MultiplayerFPS/Private/GameModes/FPSGameMode.cpp
+++ Source/MultiplayerFPS/Private/GameModes/FPSGameMode.cpp
@@ -4,6 +4,8 @@
 #include "GameModes/FPSGameMode.h"
 
 #include "Character/FPSCharacter.h"
+#include "GameFramework/PlayerStart.h"
+#include "Kismet/GameplayStatics.h"
 
 void AFPSGameMode::PlayerEliminated(AFPSCharacter* ElimmedCharacter, AFPSPlayerController* VictimController,
                                     AFPSPlayerController* AttackerController)
@@ -12,5 +14,24 @@ void AFPSGameMode::PlayerEliminated(AFPSCharacter* ElimmedCharacter, AFPSPlayerC
 	if(ElimmedCharacter)
 	{
 		ElimmedCharacter->Elim();
+	}
+}
+
+void AFPSGameMode::RequestRespawn(ACharacter* ElimmedCharacter, AController* ElimmedController)
+{
+	if(ElimmedCharacter)
+	{
+		ElimmedCharacter->Reset();	// pre-existing function that detaches the character from the controller
+		ElimmedCharacter->Destroy(); // the controller still exists though
+	}
+	if(ElimmedController)
+	{
+		// Get all Player Start actors in the level
+		TArray<AActor*> PlayerStarts;
+		UGameplayStatics::GetAllActorsOfClass(this, APlayerStart::StaticClass(), PlayerStarts);
+		int32 Selection = FMath::RandRange(0, PlayerStarts.Num() - 1); // random player start spawn
+
+		// pre-existing function that spawns a new character at a start spot
+		RestartPlayerAtPlayerStart(ElimmedController, PlayerStarts[Selection]);
 	}
 }
--- Source/MultiplayerFPS/Public/GameModes/FPSGameMode.h
+++ Source/MultiplayerFPS/Public/GameModes/FPSGameMode.h
@@ -14,5 +14,5 @@ class MULTIPLAYERFPS_API AFPSGameMode : public AGameMode
 
 public:
 	virtual void PlayerEliminated(class AFPSCharacter* ElimmedCharacter, class AFPSPlayerController* VictimController, AFPSPlayerController* AttackerController);
-	
+	virtual void RequestRespawn(ACharacter* ElimmedCharacter, AController* ElimmedController);
 };
