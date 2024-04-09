557dbda4bc848e7967813e401f34ce3102a629d4
noisfischer <noisfischer@gmail.com>
2024-04-09 10:58:56 -0400

    Add damage to Projectile base and override OnHit in ProjectileBullet

--- Source/MultiplayerFPS/Public/Weapon/Projectile.h
+++ Source/MultiplayerFPS/Public/Weapon/Projectile.h
@@ -40,6 +40,9 @@ private:
 	UPROPERTY(EditAnywhere)
 	class USoundCue* ImpactSound;
 
+	UPROPERTY(EditAnywhere)
+	float Damage;
+
 public:	
 
 };
--- Source/MultiplayerFPS/Public/Weapon/ProjectileBullet.h
+++ Source/MultiplayerFPS/Public/Weapon/ProjectileBullet.h
@@ -6,12 +6,14 @@
 #include "Weapon/Projectile.h"
 #include "ProjectileBullet.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class MULTIPLAYERFPS_API AProjectileBullet : public AProjectile
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
+
 	
 };
