bb8b5bd716220fb10cf636acbb82da7dc580f7d2
noisfischer <noisfischer@gmail.com>
2024-04-21 13:14:17 -0400

    New enum EWT_RocketLauncher for ammo init

--- Source/MultiplayerFPS/Public/Weapon/WeaponTypes.h
+++ Source/MultiplayerFPS/Public/Weapon/WeaponTypes.h
@@ -4,5 +4,6 @@ UENUM(BlueprintType)
 enum class EWeaponType : uint8
 {
 	EWT_AssaultRifle UMETA(DisplayName = "Assault Rifle"),
+	EWT_RocketLauncher UMETA(DisplayName = "Rocket Launcher"),
 	EWT_MAX UMETA(DisplayName = "DefaultMAX")
 };
