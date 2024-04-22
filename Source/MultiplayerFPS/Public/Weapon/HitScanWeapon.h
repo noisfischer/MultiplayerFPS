c726ae44715897667476d356b5b2c69dcf1e5a40
noisfischer <noisfischer@gmail.com>
2024-04-21 22:23:28 -0400

    HitScanWeapon Init

---
+++ Source/MultiplayerFPS/Public/Weapon/HitScanWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/Weapon.h"
+#include "HitScanWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERFPS_API AHitScanWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
