3c9a83d5686b8b32cfe2a884f6c0043ebaaf6bdd
noisfischer <noisfischer@gmail.com>
2024-04-23 11:19:23 -0400

    Shotgun class Init
    A hitscan weapon

---
+++ Source/MultiplayerFPS/Public/Weapon/Shotgun.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/HitScanWeapon.h"
+#include "Shotgun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERFPS_API AShotgun : public AHitScanWeapon
+{
+	GENERATED_BODY()
+	
+};
